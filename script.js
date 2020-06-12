0e41ecfb108d606bf0d04e8d6f1ff7cc0478fd99
José Neto <josenetopr5@gmail.com>
2020-06-11 21:52:54 -0300

    Adding numbers on the screen

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("input");
+const numbers = document.querySelectorAll(".number");
+const action = document.querySelectorAll(".action");
+const equal = document.getElementById("equal");
+const clear = document.getElementById("clear");
+const backspace = document.getElementById("backspace");
+
+const decimal = document.querySelector(".decimal")
+
+
+for (const number of numbers) {
+  number.addEventListener("click", handleNumber);
+}
+
+function handleNumber(event) {
+  if (input.innerHTML.length <= 13) {
+    if (input.innerHTML === "0") {
+      input.innerHTML = "";
+    }
+
+    input.innerHTML += event.target.innerHTML;
+  }
+}
